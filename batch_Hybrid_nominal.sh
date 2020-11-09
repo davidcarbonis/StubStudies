05a3aa5a46b74d004b78249b8d0ce97060582b4a
davidcarbonis <alexander.david.morton@cern.ch>
2020-11-09 10:58:04 +0000

    Update scripts

--- batch_Hybrid_nominal.sh
+++ batch_Hybrid_nominal.sh
@@ -11,16 +11,16 @@ eval `scramv1 runtime -sh`
 
 export LD_LIBRARY_PATH=`pwd`/lib/:`pwd`/lib64/:${LD_LIBRARY_PATH}
 
-cmsRun test/L1TrackNtupleMaker_cfg.py inputMC=./TTBar_PU200.txt Events=-1 output=file:./TTbar_PU200_output/outputHybridOldTight.root L1Algo=HYBRID StubWindow=OLD_TIGHT
-cmsRun test/L1TrackNtupleMaker_cfg.py inputMC=./TTBar_PU200.txt Events=-1 output=file:./TTbar_PU200_output/outputHybridOldLoose.root L1Algo=HYBRID StubWindow=OLD_LOOSE
-cmsRun test/L1TrackNtupleMaker_cfg.py inputMC=./TTBar_PU0.txt Events=-1 output=file:./TTbar_PU0_output/outputHybridOldTight.root L1Algo=HYBRID StubWindow=OLD_TIGHT
-cmsRun test/L1TrackNtupleMaker_cfg.py inputMC=./TTBar_PU0.txt Events=-1 output=file:./TTbar_PU0_output/outputHybridOldLoose.root L1Algo=HYBRID StubWindow=OLD_LOOSE
+cmsRun test/L1TrackNtupleMaker_cfg.py inputMC=./TTBar_PU200.txt Events=-1 output=file:/vols/cms/adm10/MC/TMTT/StubStudies/TTbar_PU200_output/outputHybridOldTight.root L1Algo=HYBRID StubWindow=OLD_TIGHT
+cmsRun test/L1TrackNtupleMaker_cfg.py inputMC=./TTBar_PU200.txt Events=-1 output=file:/vols/cms/adm10/MC/TMTT/StubStudies/TTbar_PU200_output/outputHybridOldLoose.root L1Algo=HYBRID StubWindow=OLD_LOOSE
+cmsRun test/L1TrackNtupleMaker_cfg.py inputMC=./TTBar_PU0.txt Events=-1 output=file:/vols/cms/adm10/MC/TMTT/StubStudies/TTbar_PU0_output/outputHybridOldTight.root L1Algo=HYBRID StubWindow=OLD_TIGHT
+cmsRun test/L1TrackNtupleMaker_cfg.py inputMC=./TTBar_PU0.txt Events=-1 output=file:/vols/cms/adm10/MC/TMTT/StubStudies/TTbar_PU0_output/outputHybridOldLoose.root L1Algo=HYBRID StubWindow=OLD_LOOSE
 
-cmsRun test/L1TrackNtupleMaker_cfg.py inputMC=./SingleElPt1p5to8_PU0.txt Events=-1 output=file:./SingleElPt1p5to8_output/outputHybridOldTight.root L1Algo=HYBRID StubWindow=OLD_TIGHT
-cmsRun test/L1TrackNtupleMaker_cfg.py inputMC=./SingleElPt1p5to8_PU0.txt Events=-1 output=file:./SingleElPt1p5to8_output/outputHybridOldLoose.root L1Algo=HYBRID StubWindow=OLD_LOOSE
+cmsRun test/L1TrackNtupleMaker_cfg.py inputMC=./SingleElPt1p5to8_PU0.txt Events=-1 output=file:/vols/cms/adm10/MC/TMTT/StubStudies/SingleElPt1p5to8_output/outputHybridOldTight.root L1Algo=HYBRID StubWindow=OLD_TIGHT
+cmsRun test/L1TrackNtupleMaker_cfg.py inputMC=./SingleElPt1p5to8_PU0.txt Events=-1 output=file:/vols/cms/adm10/MC/TMTT/StubStudies/SingleElPt1p5to8_output/outputHybridOldLoose.root L1Algo=HYBRID StubWindow=OLD_LOOSE
 
-cmsRun test/L1TrackNtupleMaker_cfg.py inputMC=./SingleMuPt1p5to8_PU0.txt Events=-1 output=file:./SingleMuPt1p5to8_output/outputHybridOldTight.root L1Algo=HYBRID StubWindow=OLD_TIGHT
-cmsRun test/L1TrackNtupleMaker_cfg.py inputMC=./SingleMuPt1p5to8_PU0.txt Events=-1 output=file:./SingleMuPt1p5to8_output/outputHybridOldLoose.root L1Algo=HYBRID StubWindow=OLD_LOOSE
+cmsRun test/L1TrackNtupleMaker_cfg.py inputMC=./SingleMuPt1p5to8_PU0.txt Events=-1 output=file:/vols/cms/adm10/MC/TMTT/StubStudies/SingleMuPt1p5to8_output/outputHybridOldTight.root L1Algo=HYBRID StubWindow=OLD_TIGHT
+cmsRun test/L1TrackNtupleMaker_cfg.py inputMC=./SingleMuPt1p5to8_PU0.txt Events=-1 output=file:/vols/cms/adm10/MC/TMTT/StubStudies/SingleMuPt1p5to8_output/outputHybridOldLoose.root L1Algo=HYBRID StubWindow=OLD_LOOSE
 
 echo "\nEnd of job on " `date` "\n"
 
