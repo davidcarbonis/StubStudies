17a3692c25d9256553b29c5068be739646eb918a
davidcarbonis <alexander.david.morton@cern.ch>
2020-12-11 21:34:49 +0000

    Update helper scripts

--- createPlots_DisplacedMu.C
+++ createPlots_DisplacedMu.C
@@ -12,12 +12,20 @@
 #.x plotMacro.C("/vols/cms/adm10/MC/TMTT/StubStudies/DisplacedMuPt1p5To8_PU0_output/StubRateToolOutput/dtc_outputTmttOldLoose.root", true)
 #.x plotMacro.C("/vols/cms/adm10/MC/TMTT/StubStudies/DisplacedMuPt1p5To8_PU0_output/StubRateToolOutput/dtc_outputTmttOldTight.root", true)
 
-.x plotMacro.C("/vols/cms/adm10/MC/TMTT/StubStudies/DisplacedMuPt1p5To8_PU0_output/StubRateToolOutput/outputHybridDefaultWindows.root", true)
-.x plotMacro.C("/vols/cms/adm10/MC/TMTT/StubStudies/DisplacedMuPt1p5To8_PU0_output/StubRateToolOutput/outputHybridOldLoose.root", true)
-.x plotMacro.C("/vols/cms/adm10/MC/TMTT/StubStudies/DisplacedMuPt1p5To8_PU0_output/StubRateToolOutput/outputHybridOldTight.root", true)
-.x plotMacro.C("/vols/cms/adm10/MC/TMTT/StubStudies/DisplacedMuPt1p5To8_PU0_output/StubRateToolOutput/outputTmttDefaultWindows.root", true)
-.x plotMacro.C("/vols/cms/adm10/MC/TMTT/StubStudies/DisplacedMuPt1p5To8_PU0_output/StubRateToolOutput/outputTmttOldLoose.root", true)
-.x plotMacro.C("/vols/cms/adm10/MC/TMTT/StubStudies/DisplacedMuPt1p5To8_PU0_output/StubRateToolOutput/outputTmttOldTight.root", true)
+#.x plotMacro.C("/vols/cms/adm10/MC/TMTT/StubStudies/DisplacedMuPt1p5To8_PU0_output/StubRateToolOutput/outputHybridDefaultWindows.root", true)
+#.x plotMacro.C("/vols/cms/adm10/MC/TMTT/StubStudies/DisplacedMuPt1p5To8_PU0_output/StubRateToolOutput/outputHybridOldLoose.root", true)
+#.x plotMacro.C("/vols/cms/adm10/MC/TMTT/StubStudies/DisplacedMuPt1p5To8_PU0_output/StubRateToolOutput/outputHybridOldTight.root", true)
+#.x plotMacro.C("/vols/cms/adm10/MC/TMTT/StubStudies/DisplacedMuPt1p5To8_PU0_output/StubRateToolOutput/outputTmttDefaultWindows.root", true)
+#.x plotMacro.C("/vols/cms/adm10/MC/TMTT/StubStudies/DisplacedMuPt1p5To8_PU0_output/StubRateToolOutput/outputTmttOldLoose.root", true)
+#.x plotMacro.C("/vols/cms/adm10/MC/TMTT/StubStudies/DisplacedMuPt1p5To8_PU0_output/StubRateToolOutput/outputTmttOldTight.root", true)
+
+.x plotMacroComparisons.C("/vols/cms/adm10/MC/TMTT/StubStudies/DisplacedMuPt1p5To8_PU0_output/StubRateToolOutput/outputHybridDefaultWindows.root", "/vols/cms/adm10/MC/TMTT/StubStudies/DisplacedMuPt1p5To8_PU0_output/StubRateToolOutput/dtc_outputHybridDefaultWindows.root", true);
+.x plotMacroComparisons.C("/vols/cms/adm10/MC/TMTT/StubStudies/DisplacedMuPt1p5To8_PU0_output/StubRateToolOutput/outputHybridOldLoose.root", "/vols/cms/adm10/MC/TMTT/StubStudies/DisplacedMuPt1p5To8_PU0_output/StubRateToolOutput/dtc_outputHybridOldLoose.root", true);
+.x plotMacroComparisons.C("/vols/cms/adm10/MC/TMTT/StubStudies/DisplacedMuPt1p5To8_PU0_output/StubRateToolOutput/outputHybridOldTight.root", "/vols/cms/adm10/MC/TMTT/StubStudies/DisplacedMuPt1p5To8_PU0_output/StubRateToolOutput/dtc_outputHybridOldTight.root", true);
+
+.x plotMacroComparisons.C("/vols/cms/adm10/MC/TMTT/StubStudies/DisplacedMuPt1p5To8_PU0_output/StubRateToolOutput/outputTmttDefaultWindows.root", "/vols/cms/adm10/MC/TMTT/StubStudies/DisplacedMuPt1p5To8_PU0_output/StubRateToolOutput/dtc_outputTmttDefaultWindows.root", true);
+.x plotMacroComparisons.C("/vols/cms/adm10/MC/TMTT/StubStudies/DisplacedMuPt1p5To8_PU0_output/StubRateToolOutput/outputTmttOldLoose.root", "/vols/cms/adm10/MC/TMTT/StubStudies/DisplacedMuPt1p5To8_PU0_output/StubRateToolOutput/dtc_outputTmttOldLoose.root", true);
+.x plotMacroComparisons.C("/vols/cms/adm10/MC/TMTT/StubStudies/DisplacedMuPt1p5To8_PU0_output/StubRateToolOutput/outputTmttOldTight.root", "/vols/cms/adm10/MC/TMTT/StubStudies/DisplacedMuPt1p5To8_PU0_output/StubRateToolOutput/dtc_outputTmttOldTight.root", true);
 
 #.x plotMacro.C("/vols/cms/adm10/MC/TMTT/StubStudies/DisplacedMuPt1p5To8_PU200_output/StubRateToolOutput/dtc_noTruncation_outputHybridDefaultWindows.root", true)
 #.x plotMacro.C("/vols/cms/adm10/MC/TMTT/StubStudies/DisplacedMuPt1p5To8_PU200_output/StubRateToolOutput/dtc_noTruncation_outputHybridOldLoose.root", true)
@@ -33,12 +41,12 @@
 #.x plotMacro.C("/vols/cms/adm10/MC/TMTT/StubStudies/DisplacedMuPt1p5To8_PU200_output/StubRateToolOutput/dtc_outputTmttOldLoose.root", true)
 #.x plotMacro.C("/vols/cms/adm10/MC/TMTT/StubStudies/DisplacedMuPt1p5To8_PU200_output/StubRateToolOutput/dtc_outputTmttOldTight.root", true)
 
-.x plotMacro.C("/vols/cms/adm10/MC/TMTT/StubStudies/DisplacedMuPt1p5To8_PU200_output/StubRateToolOutput/outputHybridDefaultWindows.root", true)
-.x plotMacro.C("/vols/cms/adm10/MC/TMTT/StubStudies/DisplacedMuPt1p5To8_PU200_output/StubRateToolOutput/outputHybridOldLoose.root", true)
-.x plotMacro.C("/vols/cms/adm10/MC/TMTT/StubStudies/DisplacedMuPt1p5To8_PU200_output/StubRateToolOutput/outputHybridOldTight.root", true)
-.x plotMacro.C("/vols/cms/adm10/MC/TMTT/StubStudies/DisplacedMuPt1p5To8_PU200_output/StubRateToolOutput/outputTmttDefaultWindows.root", true)
-.x plotMacro.C("/vols/cms/adm10/MC/TMTT/StubStudies/DisplacedMuPt1p5To8_PU200_output/StubRateToolOutput/outputTmttOldLoose.root", true)
-.x plotMacro.C("/vols/cms/adm10/MC/TMTT/StubStudies/DisplacedMuPt1p5To8_PU200_output/StubRateToolOutput/outputTmttOldTight.root", true)
+#.x plotMacro.C("/vols/cms/adm10/MC/TMTT/StubStudies/DisplacedMuPt1p5To8_PU200_output/StubRateToolOutput/outputHybridDefaultWindows.root", true)
+#.x plotMacro.C("/vols/cms/adm10/MC/TMTT/StubStudies/DisplacedMuPt1p5To8_PU200_output/StubRateToolOutput/outputHybridOldLoose.root", true)
+#.x plotMacro.C("/vols/cms/adm10/MC/TMTT/StubStudies/DisplacedMuPt1p5To8_PU200_output/StubRateToolOutput/outputHybridOldTight.root", true)
+#.x plotMacro.C("/vols/cms/adm10/MC/TMTT/StubStudies/DisplacedMuPt1p5To8_PU200_output/StubRateToolOutput/outputTmttDefaultWindows.root", true)
+#.x plotMacro.C("/vols/cms/adm10/MC/TMTT/StubStudies/DisplacedMuPt1p5To8_PU200_output/StubRateToolOutput/outputTmttOldLoose.root", true)
+#.x plotMacro.C("/vols/cms/adm10/MC/TMTT/StubStudies/DisplacedMuPt1p5To8_PU200_output/StubRateToolOutput/outputTmttOldTight.root", true)
 
 #.x plotMacro.C("/vols/cms/adm10/MC/TMTT/StubStudies/DisplacedMuPt1p5To8_PU0_output/StubRateToolOutput/dtc_noTruncation_outputHybridDefaultWindows.root", false)
 #.x plotMacro.C("/vols/cms/adm10/MC/TMTT/StubStudies/DisplacedMuPt1p5To8_PU0_output/StubRateToolOutput/dtc_noTruncation_outputHybridOldLoose.root", false)
@@ -54,12 +62,20 @@
 #.x plotMacro.C("/vols/cms/adm10/MC/TMTT/StubStudies/DisplacedMuPt1p5To8_PU0_output/StubRateToolOutput/dtc_outputTmttOldLoose.root", false)
 #.x plotMacro.C("/vols/cms/adm10/MC/TMTT/StubStudies/DisplacedMuPt1p5To8_PU0_output/StubRateToolOutput/dtc_outputTmttOldTight.root", false)
 
-.x plotMacro.C("/vols/cms/adm10/MC/TMTT/StubStudies/DisplacedMuPt1p5To8_PU0_output/StubRateToolOutput/outputHybridDefaultWindows.root", false)
-.x plotMacro.C("/vols/cms/adm10/MC/TMTT/StubStudies/DisplacedMuPt1p5To8_PU0_output/StubRateToolOutput/outputHybridOldLoose.root", false)
-.x plotMacro.C("/vols/cms/adm10/MC/TMTT/StubStudies/DisplacedMuPt1p5To8_PU0_output/StubRateToolOutput/outputHybridOldTight.root", false)
-.x plotMacro.C("/vols/cms/adm10/MC/TMTT/StubStudies/DisplacedMuPt1p5To8_PU0_output/StubRateToolOutput/outputTmttDefaultWindows.root", false)
-.x plotMacro.C("/vols/cms/adm10/MC/TMTT/StubStudies/DisplacedMuPt1p5To8_PU0_output/StubRateToolOutput/outputTmttOldLoose.root", false)
-.x plotMacro.C("/vols/cms/adm10/MC/TMTT/StubStudies/DisplacedMuPt1p5To8_PU0_output/StubRateToolOutput/outputTmttOldTight.root", false)
+#.x plotMacro.C("/vols/cms/adm10/MC/TMTT/StubStudies/DisplacedMuPt1p5To8_PU0_output/StubRateToolOutput/outputHybridDefaultWindows.root", false)
+#.x plotMacro.C("/vols/cms/adm10/MC/TMTT/StubStudies/DisplacedMuPt1p5To8_PU0_output/StubRateToolOutput/outputHybridOldLoose.root", false)
+#.x plotMacro.C("/vols/cms/adm10/MC/TMTT/StubStudies/DisplacedMuPt1p5To8_PU0_output/StubRateToolOutput/outputHybridOldTight.root", false)
+#.x plotMacro.C("/vols/cms/adm10/MC/TMTT/StubStudies/DisplacedMuPt1p5To8_PU0_output/StubRateToolOutput/outputTmttDefaultWindows.root", false)
+#.x plotMacro.C("/vols/cms/adm10/MC/TMTT/StubStudies/DisplacedMuPt1p5To8_PU0_output/StubRateToolOutput/outputTmttOldLoose.root", false)
+#.x plotMacro.C("/vols/cms/adm10/MC/TMTT/StubStudies/DisplacedMuPt1p5To8_PU0_output/StubRateToolOutput/outputTmttOldTight.root", false)
+
+.x plotMacroComparisons.C("/vols/cms/adm10/MC/TMTT/StubStudies/DisplacedMuPt1p5To8_PU0_output/StubRateToolOutput/outputHybridDefaultWindows.root", "/vols/cms/adm10/MC/TMTT/StubStudies/DisplacedMuPt1p5To8_PU0_output/StubRateToolOutput/dtc_outputHybridDefaultWindows.root", false);
+.x plotMacroComparisons.C("/vols/cms/adm10/MC/TMTT/StubStudies/DisplacedMuPt1p5To8_PU0_output/StubRateToolOutput/outputHybridOldLoose.root", "/vols/cms/adm10/MC/TMTT/StubStudies/DisplacedMuPt1p5To8_PU0_output/StubRateToolOutput/dtc_outputHybridOldLoose.root", false);
+.x plotMacroComparisons.C("/vols/cms/adm10/MC/TMTT/StubStudies/DisplacedMuPt1p5To8_PU0_output/StubRateToolOutput/outputHybridOldTight.root", "/vols/cms/adm10/MC/TMTT/StubStudies/DisplacedMuPt1p5To8_PU0_output/StubRateToolOutput/dtc_outputHybridOldTight.root", false);
+
+.x plotMacroComparisons.C("/vols/cms/adm10/MC/TMTT/StubStudies/DisplacedMuPt1p5To8_PU0_output/StubRateToolOutput/outputTmttDefaultWindows.root", "/vols/cms/adm10/MC/TMTT/StubStudies/DisplacedMuPt1p5To8_PU0_output/StubRateToolOutput/dtc_outputTmttDefaultWindows.root", false);
+.x plotMacroComparisons.C("/vols/cms/adm10/MC/TMTT/StubStudies/DisplacedMuPt1p5To8_PU0_output/StubRateToolOutput/outputTmttOldLoose.root", "/vols/cms/adm10/MC/TMTT/StubStudies/DisplacedMuPt1p5To8_PU0_output/StubRateToolOutput/dtc_outputTmttOldLoose.root", false);
+.x plotMacroComparisons.C("/vols/cms/adm10/MC/TMTT/StubStudies/DisplacedMuPt1p5To8_PU0_output/StubRateToolOutput/outputTmttOldTight.root", "/vols/cms/adm10/MC/TMTT/StubStudies/DisplacedMuPt1p5To8_PU0_output/StubRateToolOutput/dtc_outputTmttOldTight.root", false);
 
 #.x plotMacro.C("/vols/cms/adm10/MC/TMTT/StubStudies/DisplacedMuPt1p5To8_PU200_output/StubRateToolOutput/dtc_noTruncation_outputHybridDefaultWindows.root", false)
 #.x plotMacro.C("/vols/cms/adm10/MC/TMTT/StubStudies/DisplacedMuPt1p5To8_PU200_output/StubRateToolOutput/dtc_noTruncation_outputHybridOldLoose.root", false)
@@ -75,9 +91,9 @@
 #.x plotMacro.C("/vols/cms/adm10/MC/TMTT/StubStudies/DisplacedMuPt1p5To8_PU200_output/StubRateToolOutput/dtc_outputTmttOldLoose.root", false)
 #.x plotMacro.C("/vols/cms/adm10/MC/TMTT/StubStudies/DisplacedMuPt1p5To8_PU200_output/StubRateToolOutput/dtc_outputTmttOldTight.root", false)
 
-.x plotMacro.C("/vols/cms/adm10/MC/TMTT/StubStudies/DisplacedMuPt1p5To8_PU200_output/StubRateToolOutput/outputHybridDefaultWindows.root", false)
-.x plotMacro.C("/vols/cms/adm10/MC/TMTT/StubStudies/DisplacedMuPt1p5To8_PU200_output/StubRateToolOutput/outputHybridOldLoose.root", false)
-.x plotMacro.C("/vols/cms/adm10/MC/TMTT/StubStudies/DisplacedMuPt1p5To8_PU200_output/StubRateToolOutput/outputHybridOldTight.root", false)
-.x plotMacro.C("/vols/cms/adm10/MC/TMTT/StubStudies/DisplacedMuPt1p5To8_PU200_output/StubRateToolOutput/outputTmttDefaultWindows.root", false)
-.x plotMacro.C("/vols/cms/adm10/MC/TMTT/StubStudies/DisplacedMuPt1p5To8_PU200_output/StubRateToolOutput/outputTmttOldLoose.root", false)
-.x plotMacro.C("/vols/cms/adm10/MC/TMTT/StubStudies/DisplacedMuPt1p5To8_PU200_output/StubRateToolOutput/outputTmttOldTight.root", false)
+#.x plotMacro.C("/vols/cms/adm10/MC/TMTT/StubStudies/DisplacedMuPt1p5To8_PU200_output/StubRateToolOutput/outputHybridDefaultWindows.root", false)
+#.x plotMacro.C("/vols/cms/adm10/MC/TMTT/StubStudies/DisplacedMuPt1p5To8_PU200_output/StubRateToolOutput/outputHybridOldLoose.root", false)
+#.x plotMacro.C("/vols/cms/adm10/MC/TMTT/StubStudies/DisplacedMuPt1p5To8_PU200_output/StubRateToolOutput/outputHybridOldTight.root", false)
+#.x plotMacro.C("/vols/cms/adm10/MC/TMTT/StubStudies/DisplacedMuPt1p5To8_PU200_output/StubRateToolOutput/outputTmttDefaultWindows.root", false)
+#.x plotMacro.C("/vols/cms/adm10/MC/TMTT/StubStudies/DisplacedMuPt1p5To8_PU200_output/StubRateToolOutput/outputTmttOldLoose.root", false)
+#.x plotMacro.C("/vols/cms/adm10/MC/TMTT/StubStudies/DisplacedMuPt1p5To8_PU200_output/StubRateToolOutput/outputTmttOldTight.root", false)
