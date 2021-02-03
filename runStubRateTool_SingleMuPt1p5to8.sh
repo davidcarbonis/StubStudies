699350493997efba24b8b8e4fbacb2c114f30420
davidcarbonis <alexander.david.morton@cern.ch>
2021-02-03 19:03:55 +0000

    debug using temp directory to speed up batch processing

--- runStubRateTool_SingleMuPt1p5to8.sh
+++ runStubRateTool_SingleMuPt1p5to8.sh
@@ -14,50 +14,53 @@ eval `scramv1 runtime -sh`
 export LD_LIBRARY_PATH=`pwd`/lib/:`pwd`/lib64/:${LD_LIBRARY_PATH}
 
 ##
-#echo SingleMu + PU0 output
-#StubRateTool -i /vols/cms/adm10/MC/TMTT/StubStudies/SingleMuPt1p5to8_PU0_output/Hybrid_DefaultWindows/ -o $TMPDIR/outputHybridDefaultWindows.root --particle="mu"
-#StubRateTool -i /vols/cms/adm10/MC/TMTT/StubStudies/SingleMuPt1p5to8_PU0_output/Hybrid_OldLoose/ -o $TMPDIR/outputHybridOldLoose.root --particle="mu"
-#StubRateTool -i /vols/cms/adm10/MC/TMTT/StubStudies/SingleMuPt1p5to8_PU0_output/Hybrid_OldTight/ -o $TMPDIR/outputHybridOldTight.root --particle="mu"
-#StubRateTool -i /vols/cms/adm10/MC/TMTT/StubStudies/SingleMuPt1p5to8_PU0_output/Tmtt_DefaultWindows/ -o $TMPDIR/outputTmttDefaultWindows.root --particle="mu" 
-#StubRateTool -i /vols/cms/adm10/MC/TMTT/StubStudies/SingleMuPt1p5to8_PU0_output/Tmtt_OldLoose/ -o $TMPDIR/outputTmttOldLoose.root --particle="mu" 
-#StubRateTool -i /vols/cms/adm10/MC/TMTT/StubStudies/SingleMuPt1p5to8_PU0_output/Tmtt_OldTight/ -o $TMPDIR/outputTmttOldTight.root --particle="mu"
+echo SingleMu + PU0 output
+StubRateTool -i /vols/cms/adm10/MC/TMTT/StubStudies/SingleMuPt1p5to8_PU0_output/Hybrid_DefaultWindows/ -o $TMP/outputHybridDefaultWindows.root --particle="mu"
+#StubRateTool -i /vols/cms/adm10/MC/TMTT/StubStudies/SingleMuPt1p5to8_PU0_output/Hybrid_OldLoose/ -o $TMP/outputHybridOldLoose.root --particle="mu"
+#StubRateTool -i /vols/cms/adm10/MC/TMTT/StubStudies/SingleMuPt1p5to8_PU0_output/Hybrid_OldTight/ -o $TMP/outputHybridOldTight.root --particle="mu"
+#StubRateTool -i /vols/cms/adm10/MC/TMTT/StubStudies/SingleMuPt1p5to8_PU0_output/Tmtt_DefaultWindows/ -o $TMP/outputTmttDefaultWindows.root --particle="mu" 
+#StubRateTool -i /vols/cms/adm10/MC/TMTT/StubStudies/SingleMuPt1p5to8_PU0_output/Tmtt_OldLoose/ -o $TMP/outputTmttOldLoose.root --particle="mu" 
+#StubRateTool -i /vols/cms/adm10/MC/TMTT/StubStudies/SingleMuPt1p5to8_PU0_output/Tmtt_OldTight/ -o $TMP/outputTmttOldTight.root --particle="mu"
 #echo SingleMu output + PU0 + dtc emulation
-#StubRateTool -i /vols/cms/adm10/MC/TMTT/StubStudies/SingleMuPt1p5to8_PU0_output/dtc_Hybrid_DefaultWindows/ -o $TMPDIR/dtc_outputHybridDefaultWindows.root --particle="mu"
-#StubRateTool -i /vols/cms/adm10/MC/TMTT/StubStudies/SingleMuPt1p5to8_PU0_output/dtc_Hybrid_OldLoose/ -o $TMPDIR/dtc_outputHybridOldLoose.root --particle="mu"
-#StubRateTool -i /vols/cms/adm10/MC/TMTT/StubStudies/SingleMuPt1p5to8_PU0_output/dtc_Hybrid_OldTight/ -o $TMPDIR/dtc_outputHybridOldTight.root --particle="mu"
-#StubRateTool -i /vols/cms/adm10/MC/TMTT/StubStudies/SingleMuPt1p5to8_PU0_output/dtc_Tmtt_DefaultWindows/ -o $TMPDIR/dtc_outputTmttDefaultWindows.root --particle="mu"
-#StubRateTool -i /vols/cms/adm10/MC/TMTT/StubStudies/SingleMuPt1p5to8_PU0_output/dtc_Tmtt_OldLoose/ -o $TMPDIR/dtc_outputTmttOldLoose.root --particle="mu"
-#StubRateTool -i /vols/cms/adm10/MC/TMTT/StubStudies/SingleMuPt1p5to8_PU0_output/dtc_Tmtt_OldTight/ -o $TMPDIR/dtc_outputTmttOldTight.root --particle="mu"
-#StubRateTool -i /vols/cms/adm10/MC/TMTT/StubStudies/SingleMuPt1p5to8_PU0_output/dtc_Hybrid_DefaultWindows_noTruncation/ -o $TMPDIR/dtc_noTruncation_outputHybridDefaultWindows.root --particle="mu"
-#StubRateTool -i /vols/cms/adm10/MC/TMTT/StubStudies/SingleMuPt1p5to8_PU0_output/dtc_Hybrid_OldLoose_noTruncation/ -o $TMPDIR/dtc_noTruncation_outputHybridOldLoose.root --particle="mu"
-#StubRateTool -i /vols/cms/adm10/MC/TMTT/StubStudies/SingleMuPt1p5to8_PU0_output/dtc_Hybrid_OldTight_noTruncation/ -o $TMPDIR/dtc_noTruncation_outputHybridOldTight.root --particle="mu"
-#StubRateTool -i /vols/cms/adm10/MC/TMTT/StubStudies/SingleMuPt1p5to8_PU0_output/dtc_Tmtt_DefaultWindows_noTruncation/ -o $TMPDIR/dtc_noTruncation_outputTmttDefaultWindows.root --particle="mu"
-#StubRateTool -i /vols/cms/adm10/MC/TMTT/StubStudies/SingleMuPt1p5to8_PU0_output/dtc_Tmtt_OldLoose_noTruncation/ -o $TMPDIR/dtc_noTruncation_outputTmttOldLoose.root --particle="mu"
-#StubRateTool -i /vols/cms/adm10/MC/TMTT/StubStudies/SingleMuPt1p5to8_PU0_output/dtc_Tmtt_OldTight_noTruncation/ -o $TMPDIR/dtc_noTruncation_outputTmttOldTight.root --particle="mu"
+#StubRateTool -i /vols/cms/adm10/MC/TMTT/StubStudies/SingleMuPt1p5to8_PU0_output/dtc_Hybrid_DefaultWindows/ -o $TMP/dtc_outputHybridDefaultWindows.root --particle="mu"
+#StubRateTool -i /vols/cms/adm10/MC/TMTT/StubStudies/SingleMuPt1p5to8_PU0_output/dtc_Hybrid_OldLoose/ -o $TMP/dtc_outputHybridOldLoose.root --particle="mu"
+#StubRateTool -i /vols/cms/adm10/MC/TMTT/StubStudies/SingleMuPt1p5to8_PU0_output/dtc_Hybrid_OldTight/ -o $TMP/dtc_outputHybridOldTight.root --particle="mu"
+#StubRateTool -i /vols/cms/adm10/MC/TMTT/StubStudies/SingleMuPt1p5to8_PU0_output/dtc_Tmtt_DefaultWindows/ -o $TMP/dtc_outputTmttDefaultWindows.root --particle="mu"
+#StubRateTool -i /vols/cms/adm10/MC/TMTT/StubStudies/SingleMuPt1p5to8_PU0_output/dtc_Tmtt_OldLoose/ -o $TMP/dtc_outputTmttOldLoose.root --particle="mu"
+#StubRateTool -i /vols/cms/adm10/MC/TMTT/StubStudies/SingleMuPt1p5to8_PU0_output/dtc_Tmtt_OldTight/ -o $TMP/dtc_outputTmttOldTight.root --particle="mu"
+#StubRateTool -i /vols/cms/adm10/MC/TMTT/StubStudies/SingleMuPt1p5to8_PU0_output/dtc_Hybrid_DefaultWindows_noTruncation/ -o $TMP/dtc_noTruncation_outputHybridDefaultWindows.root --particle="mu"
+#StubRateTool -i /vols/cms/adm10/MC/TMTT/StubStudies/SingleMuPt1p5to8_PU0_output/dtc_Hybrid_OldLoose_noTruncation/ -o $TMP/dtc_noTruncation_outputHybridOldLoose.root --particle="mu"
+#StubRateTool -i /vols/cms/adm10/MC/TMTT/StubStudies/SingleMuPt1p5to8_PU0_output/dtc_Hybrid_OldTight_noTruncation/ -o $TMP/dtc_noTruncation_outputHybridOldTight.root --particle="mu"
+#StubRateTool -i /vols/cms/adm10/MC/TMTT/StubStudies/SingleMuPt1p5to8_PU0_output/dtc_Tmtt_DefaultWindows_noTruncation/ -o $TMP/dtc_noTruncation_outputTmttDefaultWindows.root --particle="mu"
+#StubRateTool -i /vols/cms/adm10/MC/TMTT/StubStudies/SingleMuPt1p5to8_PU0_output/dtc_Tmtt_OldLoose_noTruncation/ -o $TMP/dtc_noTruncation_outputTmttOldLoose.root --particle="mu"
+#StubRateTool -i /vols/cms/adm10/MC/TMTT/StubStudies/SingleMuPt1p5to8_PU0_output/dtc_Tmtt_OldTight_noTruncation/ -o $TMP/dtc_noTruncation_outputTmttOldTight.root --particle="mu"
 #
 #echo SingleMu + PU200 output
-#StubRateTool -i /vols/cms/adm10/MC/TMTT/StubStudies/SingleMuPt1p5to8_PU200_output/Hybrid_DefaultWindows/ -o $TMPDIR/outputHybridDefaultWindows.root --particle="mu"
-#StubRateTool -i /vols/cms/adm10/MC/TMTT/StubStudies/SingleMuPt1p5to8_PU200_output/Hybrid_OldLoose/ -o $TMPDIR/outputHybridOldLoose.root --particle="mu"
-#StubRateTool -i /vols/cms/adm10/MC/TMTT/StubStudies/SingleMuPt1p5to8_PU200_output/Hybrid_OldTight/ -o $TMPDIR/outputHybridOldTight.root --particle="mu"
-#StubRateTool -i /vols/cms/adm10/MC/TMTT/StubStudies/SingleMuPt1p5to8_PU200_output/Tmtt_DefaultWindows/ -o $TMPDIR/outputTmttDefaultWindows.root --particle="mu" 
-#StubRateTool -i /vols/cms/adm10/MC/TMTT/StubStudies/SingleMuPt1p5to8_PU200_output/Tmtt_OldLoose/ -o $TMPDIR/outputTmttOldLoose.root --particle="mu" 
-#StubRateTool -i /vols/cms/adm10/MC/TMTT/StubStudies/SingleMuPt1p5to8_PU200_output/Tmtt_OldTight/ -o $TMPDIR/outputTmttOldTight.root --particle="mu"
+#StubRateTool -i /vols/cms/adm10/MC/TMTT/StubStudies/SingleMuPt1p5to8_PU200_output/Hybrid_DefaultWindows/ -o $TMP/outputHybridDefaultWindows.root --particle="mu"
+#StubRateTool -i /vols/cms/adm10/MC/TMTT/StubStudies/SingleMuPt1p5to8_PU200_output/Hybrid_OldLoose/ -o $TMP/outputHybridOldLoose.root --particle="mu"
+#StubRateTool -i /vols/cms/adm10/MC/TMTT/StubStudies/SingleMuPt1p5to8_PU200_output/Hybrid_OldTight/ -o $TMP/outputHybridOldTight.root --particle="mu"
+#StubRateTool -i /vols/cms/adm10/MC/TMTT/StubStudies/SingleMuPt1p5to8_PU200_output/Tmtt_DefaultWindows/ -o $TMP/outputTmttDefaultWindows.root --particle="mu" 
+#StubRateTool -i /vols/cms/adm10/MC/TMTT/StubStudies/SingleMuPt1p5to8_PU200_output/Tmtt_OldLoose/ -o $TMP/outputTmttOldLoose.root --particle="mu" 
+#StubRateTool -i /vols/cms/adm10/MC/TMTT/StubStudies/SingleMuPt1p5to8_PU200_output/Tmtt_OldTight/ -o $TMP/outputTmttOldTight.root --particle="mu"
 #echo SingleMu output + PU200 + dtc emulation
-#StubRateTool -i /vols/cms/adm10/MC/TMTT/StubStudies/SingleMuPt1p5to8_PU200_output/dtc_Hybrid_DefaultWindows/ -o $TMPDIR/dtc_outputHybridDefaultWindows.root --particle="mu"
-#StubRateTool -i /vols/cms/adm10/MC/TMTT/StubStudies/SingleMuPt1p5to8_PU200_output/dtc_Hybrid_OldLoose/ -o $TMPDIR/dtc_outputHybridOldLoose.root --particle="mu"
-#StubRateTool -i /vols/cms/adm10/MC/TMTT/StubStudies/SingleMuPt1p5to8_PU200_output/dtc_Hybrid_OldTight/ -o $TMPDIR/dtc_outputHybridOldTight.root --particle="mu"
-#StubRateTool -i /vols/cms/adm10/MC/TMTT/StubStudies/SingleMuPt1p5to8_PU200_output/dtc_Tmtt_DefaultWindows/ -o $TMPDIR/dtc_outputTmttDefaultWindows.root --particle="mu"
-#StubRateTool -i /vols/cms/adm10/MC/TMTT/StubStudies/SingleMuPt1p5to8_PU200_output/dtc_Tmtt_OldLoose/ -o $TMPDIR/dtc_outputTmttOldLoose.root --particle="mu"
-#StubRateTool -i /vols/cms/adm10/MC/TMTT/StubStudies/SingleMuPt1p5to8_PU200_output/dtc_Tmtt_OldTight/ -o $TMPDIR/dtc_outputTmttOldTight.root --particle="mu"
-#StubRateTool -i /vols/cms/adm10/MC/TMTT/StubStudies/SingleMuPt1p5to8_PU200_output/dtc_Hybrid_DefaultWindows_noTruncation/ -o $TMPDIR/dtc_noTruncation_outputHybridDefaultWindows.root --particle="mu"
-#StubRateTool -i /vols/cms/adm10/MC/TMTT/StubStudies/SingleMuPt1p5to8_PU200_output/dtc_Hybrid_OldLoose_noTruncation/ -o $TMPDIR/dtc_noTruncation_outputHybridOldLoose.root --particle="mu"
-#StubRateTool -i /vols/cms/adm10/MC/TMTT/StubStudies/SingleMuPt1p5to8_PU200_output/dtc_Hybrid_OldTight_noTruncation/ -o $TMPDIR/dtc_noTruncation_outputHybridOldTight.root --particle="mu"
-#StubRateTool -i /vols/cms/adm10/MC/TMTT/StubStudies/SingleMuPt1p5to8_PU200_output/dtc_Tmtt_DefaultWindows_noTruncation/ -o $TMPDIR/dtc_noTruncation_outputTmttDefaultWindows.root --particle="mu"
-#StubRateTool -i /vols/cms/adm10/MC/TMTT/StubStudies/SingleMuPt1p5to8_PU200_output/dtc_Tmtt_OldLoose_noTruncation/ -o $TMPDIR/dtc_noTruncation_outputTmttOldLoose.root --particle="mu"
-#StubRateTool -i /vols/cms/adm10/MC/TMTT/StubStudies/SingleMuPt1p5to8_PU200_output/dtc_Tmtt_OldTight_noTruncation/ -o $TMPDIR/dtc_noTruncation_outputTmttOldTight.root --particle="mu"
+#StubRateTool -i /vols/cms/adm10/MC/TMTT/StubStudies/SingleMuPt1p5to8_PU200_output/dtc_Hybrid_DefaultWindows/ -o $TMP/dtc_outputHybridDefaultWindows.root --particle="mu"
+#StubRateTool -i /vols/cms/adm10/MC/TMTT/StubStudies/SingleMuPt1p5to8_PU200_output/dtc_Hybrid_OldLoose/ -o $TMP/dtc_outputHybridOldLoose.root --particle="mu"
+#StubRateTool -i /vols/cms/adm10/MC/TMTT/StubStudies/SingleMuPt1p5to8_PU200_output/dtc_Hybrid_OldTight/ -o $TMP/dtc_outputHybridOldTight.root --particle="mu"
+#StubRateTool -i /vols/cms/adm10/MC/TMTT/StubStudies/SingleMuPt1p5to8_PU200_output/dtc_Tmtt_DefaultWindows/ -o $TMP/dtc_outputTmttDefaultWindows.root --particle="mu"
+#StubRateTool -i /vols/cms/adm10/MC/TMTT/StubStudies/SingleMuPt1p5to8_PU200_output/dtc_Tmtt_OldLoose/ -o $TMP/dtc_outputTmttOldLoose.root --particle="mu"
+#StubRateTool -i /vols/cms/adm10/MC/TMTT/StubStudies/SingleMuPt1p5to8_PU200_output/dtc_Tmtt_OldTight/ -o $TMP/dtc_outputTmttOldTight.root --particle="mu"
+#StubRateTool -i /vols/cms/adm10/MC/TMTT/StubStudies/SingleMuPt1p5to8_PU200_output/dtc_Hybrid_DefaultWindows_noTruncation/ -o $TMP/dtc_noTruncation_outputHybridDefaultWindows.root --particle="mu"
+#StubRateTool -i /vols/cms/adm10/MC/TMTT/StubStudies/SingleMuPt1p5to8_PU200_output/dtc_Hybrid_OldLoose_noTruncation/ -o $TMP/dtc_noTruncation_outputHybridOldLoose.root --particle="mu"
+#StubRateTool -i /vols/cms/adm10/MC/TMTT/StubStudies/SingleMuPt1p5to8_PU200_output/dtc_Hybrid_OldTight_noTruncation/ -o $TMP/dtc_noTruncation_outputHybridOldTight.root --particle="mu"
+#StubRateTool -i /vols/cms/adm10/MC/TMTT/StubStudies/SingleMuPt1p5to8_PU200_output/dtc_Tmtt_DefaultWindows_noTruncation/ -o $TMP/dtc_noTruncation_outputTmttDefaultWindows.root --particle="mu"
+#StubRateTool -i /vols/cms/adm10/MC/TMTT/StubStudies/SingleMuPt1p5to8_PU200_output/dtc_Tmtt_OldLoose_noTruncation/ -o $TMP/dtc_noTruncation_outputTmttOldLoose.root --particle="mu"
+#StubRateTool -i /vols/cms/adm10/MC/TMTT/StubStudies/SingleMuPt1p5to8_PU200_output/dtc_Tmtt_OldTight_noTruncation/ -o $TMP/dtc_noTruncation_outputTmttOldTight.root --particle="mu"
 
-cp $TMPDIR/*.root    /vols/cms/adm10/MC/TMTT/StubStudies/SingleMuPt1p5To8_PU0_output/StubRateToolOutput/
-# cp $TMPDIR/*.root  /vols/cms/adm10/MC/TMTT/StubStudies/SingleMuPt1p5To8_PU200_output/StubRateToolOutput/
+echo "ls $TMP/*.root"
+ls $TMP/*.root
+echo "cp $TMP/* /vols/cms/adm10/MC/TMTT/StubStudies/SingleMuPt1p5to8_PU0_output/StubRateToolOutput/"
+cp $TMP/* /vols/cms/adm10/MC/TMTT/StubStudies/SingleMuPt1p5to8_PU0_output/StubRateToolOutput/ -v
+#cp $TMP/*.root  /vols/cms/adm10/MC/TMTT/StubStudies/SingleMuPt1p5To8_PU200_output/StubRateToolOutput/ -v
 
 echo "\nEnd of job on " `date` "\n"
 
